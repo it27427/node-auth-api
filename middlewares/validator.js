839515758ee26d879ff9abe452853c1e32065118
it27427 <istiak.tushar.qtec@gmail.com>
2025-07-31 00:25:33 +0600

    signUpSchema email vaidation updated

--- middlewares/validator.js
+++ middlewares/validator.js
@@ -2,7 +2,13 @@ const Joi = require("joi");
 
 const signUpSchema = Joi.object({
   username: Joi.string().min(3).max(40).required().trim(),
-  email: Joi.string().min(6).max(50).email().required().trim().lowercase(),
+  email: Joi.string()
+    .min(6)
+    .max(50)
+    .email({ tlds: { allow: ["com", "net"] } })
+    .required()
+    .trim()
+    .lowercase(),
   phoneNumber: Joi.string().min(11).required(),
   password: Joi.string()
     .min(6)
