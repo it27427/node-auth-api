8cf29fdc35627507628d80dd441734a67c98fc55
it27427 <istiak.tushar.qtec@gmail.com>
2025-07-30 23:02:45 +0600

    signUp Functionality

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,9 +1,11 @@
 const signUp = async (req, res) => {
+  const { username, email, phoneNumber, password, confirmPassword } = req.body;
+
   try {
     // Handle user signup logic here
     res.status(201).json({ message: "User signed up successfully" });
   } catch (error) {
-    res.status(500).json({ error: "Internal server error" });
+    res.status(500).json({ error: "Internal server error" }, error);
   }
 };
 
